77c9197250e6aa347cc46b55228707eccdb702d0
catherline nyanduko mirobe <catherline.nyanduko@student.moringaschool.com>
2022-08-18 10:32:59 +0300

    commit tree 3f93a21a62824e114189af7755a7f1a4b3d0042a

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+def badge_maker name
+     "Hello, my name is #{name}."
+end
+def batch_badge_creator array
+   new_array=[]
+    array.each do |name|
+   new_array.push ("Hello, my name is #{name}.")
+    end
+    new_array
+end
+def assign_rooms array
+    new_arra1=[]
+    array.each.with_index do |name,index|
+    new_arra1.push("Hello, #{name}! You'll be assigned to room #{index+1}!") 
+    end
+    new_arra1
+end
